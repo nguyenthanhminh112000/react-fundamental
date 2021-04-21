6ca584b3d7012be56c76e3e079d5401cfe1e0b9e
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-04-21 15:16:32 +0700

    Thinking in React

--- src/index.js
+++ src/index.js
@@ -327,25 +327,194 @@ import reportWebVitals from './reportWebVitals';
 // }
 // ReactDOM.render(<Calculator />, document.getElementById('root'));
 
+// ////////////////////////////////////////// COMPOSITION AND INHERITANCE
+// class ParentComponent extends React.Component {
+//   render() {
+//     return <div>{this.props.children}</div>;
+//   }
+// }
 
-////////////////////////////////////////// COMPOSITION AND INHERITANCE
-class ParentComponent extends React.Component {
-  render() {
-    return <div>{this.props.children}</div>;
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <ParentComponent>
+//         Hello world. My name is Nguyen Thanh Minh. I'm 21 current self-taught
+//         developer. Nice to meet you
+//       </ParentComponent>
+//     );
+//   }
+// }
 
-class App extends React.Component {
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+////////////////////////////////////////// THINKING IN REACT
+const PRODUCTS = [
+  {
+    category: 'Sporting Goods',
+    price: '$49.99',
+    stocked: true,
+    name: 'Football',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99',
+    stocked: true,
+    name: 'Baseball',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99',
+    stocked: false,
+    name: 'Basketball',
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99',
+    stocked: true,
+    name: 'iPod Touch',
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99',
+    stocked: false,
+    name: 'iPhone 5',
+  },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+];
+
+class ProductCategoryRow extends React.Component {
   render() {
+    const category = this.props.category;
     return (
-      <ParentComponent>
-        Hello world. My name is Nguyen Thanh Minh. I'm 21 current self-taught
-        developer. Nice to meet you
-      </ParentComponent>
+      <tr>
+        <th colSpan='2'>{category}</th>
+      </tr>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? (
+      product.name
+    ) : (
+      <span style={{ color: 'red' }}>{product.name}</span>
+    );
 
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+    const rows = [];
+    let lastCategory = null;
+    // Fulfill the qualified products
+    this.props.products.forEach((product) => {
+      // filter the unnecessary product
+      if (product.name.indexOf(filterText) === -1) {
+        console.log('b');
+        return;
+      }
+      // filter the products that are not in the Stock
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category}
+          />
+        );
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  handleFilterTextChange = (e) => {
+    this.props.onFilterTextChange(e.target.value);
+  };
+  handleInStockChange = (e) => {
+    this.props.onInStockChange();
+  };
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='Search...'
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type='checkbox'
+            value={this.props.inStockOnly}
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockChange}
+          />{' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { filterText: '', inStockOnly: false };
+  }
+  handleFilterTextChange = (filterText) => {
+    this.setState({ filterText: filterText });
+  };
+  handleInStockChange = () => {
+    this.setState({ inStockOnly: !this.state.inStockOnly });
+  };
+  render() {
+    return (
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('root')
+);
 reportWebVitals();
